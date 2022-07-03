c1a557142039f77f63ab0fd1b6be880c8bd1a0e8
Techypanda <Jonathan_wright@hotmail.com>
2022-07-03 16:21:54 +0800

    basic docker setup for web

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    watch: {
+      usePolling: true
+    }
+  },
+  build: {
+    outDir: "dist",
+  },
   plugins: [react()]
 })
